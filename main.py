aa183e5a5bca6f75c5aa6ba7cc895c5a01d7ee1a
Kadzuhiro09 <milavenger44@gmail.com>
2025-02-20 17:58:52 +0200

    ultra mega pasword generator version 0.1.48.7. alphabeta1

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+from ui import Ui_MainWindow
+
+class Widget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.pushButton.clicked.connect(self.example)
+
+    def example(self):
+        signs =''
+        if self.ui.checkBox.isChecked():
+            signs = 'qwertyuiopasdfghjklzxcvbnm'
+        if self.ui.checkBox_2.isChecked():
+            signs += '1234567890'
+        
+        result = ''
+        number = random.randint(5, 10)
+        for i in range (number):
+            result += random.choice(signs)
+
+        self.ui.label_2.setText(result)
+        
+app = QApplication([])
+ex = Widget()
+ex.show()
+app.exec_()
